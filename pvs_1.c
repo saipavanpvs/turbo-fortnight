71e96a35ef234f23ee69b14805764a169236d780
saipavanpvs <52665572+saipavanpvs@users.noreply.github.com>
2019-07-08 21:09:30 +0530

    Create pvs_1.c

---
+++ pvs_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    if(n==0)
+printf("zero");
+else if(n>0)
+printf("positive");
+else
+printf("negative");
+}
