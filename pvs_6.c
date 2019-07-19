bc4b5fbf34ac489e74e505c9d942961c03e23337
saipavanpvs <52665572+saipavanpvs@users.noreply.github.com>
2019-07-19 13:53:07 +0530

    Create pvs_6.c

---
+++ pvs_6.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include<stdio.h>
+void main()
+{
+int i;
+char c;
+scanf("%c",&c);
+i=c;
+if(i>96||i<122||i>65||i<90)
+printf("Alphabet");
+else 
+printf("No");
+
+}
+
