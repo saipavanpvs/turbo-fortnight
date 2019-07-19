a368ac1427729ecd1fb34f2cc9a32bcac76d6b2b
saipavanpvs <52665572+saipavanpvs@users.noreply.github.com>
2019-07-19 13:49:00 +0530

    Create pvs_5.c

---
+++ pvs_5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int i;
+char c;
+scanf("%c",&c);
+i=c;
+if(i==97||i==101||i==105||i==111||i==117)
+printf("vowel");
+else if(i<96||i>122)
+printf("invalid");
+else
+printf("Consonant");
+}
