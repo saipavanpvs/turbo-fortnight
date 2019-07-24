55a782754ecd99bf65f997259c72820c355d0c25
saipavanpvs <52665572+saipavanpvs@users.noreply.github.com>
2019-07-24 09:46:16 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 void main()
 {
-    int a,i;
+    int a,ic=0;
     scanf("%d",&a);
     for(i=2;i<a;i++)
+        
     {
         if(a%i==0)
+            c++;
+    }
+        if(c==2)
         printf("no");
         
         else
         printf("yes");
-        break;
-    }
+        
+    
 }
