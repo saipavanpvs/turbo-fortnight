fdf310d3d0aa679ae179fdeddb16d6f8e4cc0087
saipavanpvs <52665572+saipavanpvs@users.noreply.github.com>
2019-07-25 19:19:26 +0530

    Update and rename prime.c to prime1.c

---
+++ prime1.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+ int main()
+ {
+   int i, prime,a,b, n;
+   scanf("%d", &a);
+   scanf("%d", &b);
+   for(n=a+1; n<b; n++)
+   {
+     prime = 1;
+     for(i=2; i<n/2; i++)
+       if(n%i == 0)
+       {
+         prime = 0;
+         break;
+       }
+     if(prime)
+       printf("%d", n);
+   }
+ }
